5711ff6251f89b4ca16ce512e377db5bfdd0b611
MohammadArik <mohammadarik63@gmail.com>
2022-08-03 08:55:11 +0600

    top appbar added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+final Uri supportUri = Uri.parse('https://www.buymeacoffee.com/wordlys');
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  Future<void> handleSupportUsClick() async {
+    await launchUrl(
+      supportUri,
+      mode: LaunchMode.externalApplication,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text("Testing...")),
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Container(
+              padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+              child: Row(
+                children: <Widget>[
+                  const Image(
+                    image: AssetImage("assets/images/logo.png"),
+                    width: 48,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  const Text(
+                    "Wordlys",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  const Expanded(
+                    child: SizedBox(),
+                  ),
+                  TextButton(
+                      onPressed: handleSupportUsClick,
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                              const Color(0xFF7B67FD)),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              Color(0xFFFFFFFF)),
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 12))),
+                      child: Row(
+                        children: const [
+                          Image(image: AssetImage("assets/images/support.png")),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text("Support Us"),
+                        ],
+                      ))
+                ],
+              ))
+        ],
+      )),
     );
   }
 }

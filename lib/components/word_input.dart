ec2b17971a4a593b1aa5477b46cfa0cac12f992f
MohammadArik <mohammadarik63@gmail.com>
2022-08-04 18:11:33 +0600

    expanded removed

--- lib/components/word_input.dart
+++ lib/components/word_input.dart
@@ -11,50 +11,48 @@ class WordInput extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-        color: const Color(PRIMARY),
-        alignment: Alignment.topLeft,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              child: TextField(
-                controller: apiHandler.textFieldController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: 'Search by word...',
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: EdgeInsets.all(16),
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+      color: const Color(PRIMARY),
+      alignment: Alignment.topLeft,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: TextField(
+              controller: apiHandler.textFieldController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  borderSide: BorderSide.none,
                 ),
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Color(PRIMARY_TEXT),
-                  fontWeight: FontWeight.bold,
-                ),
+                hintText: 'Search by word...',
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: EdgeInsets.all(16),
+              ),
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color(PRIMARY_TEXT),
+                fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(width: 8),
-            TextButton(
-                onPressed: (() {}),
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Color(HIGHLIGHT)),
-                  padding:
-                      MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(17)),
-                ),
-                child: Image(
-                  image: AssetImage("assets/images/search.png"),
-                  width: 25,
-                  height: 25,
-                )),
-          ],
-        ),
+          ),
+          SizedBox(width: 8),
+          TextButton(
+              onPressed: (() {}),
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Color(HIGHLIGHT)),
+                padding:
+                    MaterialStateProperty.all<EdgeInsets>(EdgeInsets.all(17)),
+              ),
+              child: Image(
+                image: AssetImage("assets/images/search.png"),
+                width: 25,
+                height: 25,
+              )),
+        ],
       ),
     );
   }

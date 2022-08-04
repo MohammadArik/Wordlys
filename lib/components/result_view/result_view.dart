4593d043e98e9250b4b1a9580321c12fc29e0849
MohammadArik <mohammadarik63@gmail.com>
2022-08-04 19:52:57 +0600

    primary work done and relocated

---
+++ lib/components/result_view/result_view.dart
@@ -0,0 +1,62 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:wordlys/bloc/api_handler.dart';
+import 'package:wordlys/const/colors.dart';
+
+class ResultView extends StatelessWidget {
+  const ResultView({required this.apiHandler});
+  final APIHandler apiHandler;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        width: window.physicalSize.width,
+        color: Color(PRIMARY),
+        padding: EdgeInsets.symmetric(horizontal: 32),
+        child: StreamBuilder<WordData>(
+          stream: apiHandler.getData,
+          builder: ((context, snapshot) {
+            var data = snapshot.data;
+
+            print(data);
+            if (data != null && data.state != DataState.Empty) {
+              //* Check if loading
+              if (data.state == DataState.Loading) {
+                return Text(
+                  "Loading...",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(PRIMARY_TEXT),
+                    fontSize: 15,
+                  ),
+                );
+              }
+
+              //* Check if the result is not found
+              if (data.state == DataState.NotFound) {
+                return Text(
+                  "Can't find any results.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(PRIMARY_TEXT),
+                    fontSize: 15,
+                  ),
+                );
+              }
+
+              //* It is now surely ready
+
+              // TODO do stuff with the data (not implemented yet)
+              return Text("Result Data: " + data.mainWord);
+            }
+
+            return SizedBox();
+          }),
+        ),
+      ),
+    );
+  }
+}
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wordlys/bloc/api_handler.dart';
-import 'package:wordlys/components/result_view.dart';
+import 'package:wordlys/components/result_view/result_view.dart';
 import 'package:wordlys/components/top_appbar.dart';
 import 'package:wordlys/components/word_input.dart';
 

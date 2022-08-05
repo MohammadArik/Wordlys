d052a4c9c2883ad1e4c1466362aeee5864bb95e4
MohammadArik <mohammadarik63@gmail.com>
2022-08-05 12:55:07 +0600

    synonym and antonym showing widget function added

--- lib/components/result_view/meaning_block.dart
+++ lib/components/result_view/meaning_block.dart
@@ -14,9 +14,16 @@ class _MeaningBlockState extends State<MeaningBlock> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color(0xFFFFFFFF),
       padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Color(0xFFFFFFFF),
+        border: Border.all(
+          color: Color(BORDER),
+          width: 1,
+        ),
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             children: [
@@ -54,14 +61,87 @@ class _MeaningBlockState extends State<MeaningBlock> {
                         "assets/images/upArrow.png",
                         width: 18,
                       ),
-                color: Color(0xFFEEEEEE),
-                splashColor: Color(0xFFEEEEEE),
+                color: Color(BORDER),
+                splashColor: Color(BORDER),
                 padding: EdgeInsets.all(4),
               ),
             ],
-          )
+          ),
+          isExpanded
+              ? Column(
+                  children: [
+                    SizedBox(height: 8),
+                    wordsSection([
+                      "Word 1",
+                      "Word 2",
+                      "Word 3",
+                      "Word 4",
+                    ], WordType.Synonym),
+                    SizedBox(height: 8),
+                    wordsSection([
+                      "Word 1",
+                      "Word 2",
+                      "Word 3",
+                      "Word 4",
+                    ], WordType.Antonym)
+                  ],
+                )
+              : SizedBox()
         ],
       ),
     );
   }
+}
+
+enum WordType { Synonym, Antonym }
+
+Widget wordsSection(List<String> wordList, WordType type) {
+  final blockColorNum = type == WordType.Synonym ? HIGHLIGHT : 0xFF0957B7;
+
+  List<Widget> wordsWidgetList = <Widget>[];
+  for (var word in wordList) {
+    wordsWidgetList.add(
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+        margin: EdgeInsets.fromLTRB(0, 4, 8, 4),
+        decoration: BoxDecoration(
+            color: Color(blockColorNum),
+            borderRadius: BorderRadius.all(Radius.circular(4))),
+        child: Text(
+          word,
+          style: TextStyle(
+            color: Color(0xFFF9F8FF),
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  return Container(
+    width: Size.infinite.width,
+    decoration: BoxDecoration(
+      color: Color(0xFFF9F8FF),
+      border: Border.all(
+        color: Color(BORDER),
+      ),
+    ),
+    padding: EdgeInsets.all(12),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          (type == WordType.Synonym ? "Synonyms" : "Antonyms"),
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+        ),
+        Wrap(
+          children: wordsWidgetList,
+        )
+      ],
+    ),
+  );
 }

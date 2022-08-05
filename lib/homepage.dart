bf8789ac251fa0d35595e786c663d75dc66ac9e3
MohammadArik <mohammadarik63@gmail.com>
2022-08-05 16:09:28 +0600

    redundant print removed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,7 +11,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print("I am rendered");
     return Scaffold(
       body: SafeArea(
           child: Column(

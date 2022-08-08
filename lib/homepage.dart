dfaf28708c519848fb21c7bab39c307c52625b26
MohammadArik <mohammadarik63@gmail.com>
2022-08-08 23:41:23 +0600

    legacy APIHandler removed

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wordlys/bloc/api_handler.dart';
 import 'package:wordlys/components/result_view/result_view.dart';
 import 'package:wordlys/components/top_appbar.dart';
 import 'package:wordlys/components/word_input.dart';
@@ -7,21 +6,16 @@ import 'package:wordlys/components/word_input.dart';
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
-  final apiHandler = APIHandler();
-
   @override
   Widget build(BuildContext context) {
+    print("I am rerendered :(");
     return Scaffold(
       body: SafeArea(
           child: Column(
         children: [
           const TopAppBar(),
-          WordInput(
-            apiHandler: apiHandler,
-          ),
-          ResultView(
-            apiHandler: apiHandler,
-          ),
+          WordInput(),
+          ResultView(),
         ],
       )),
     );

71264a69b8320102ffafe4a9db3dcc5787f08c5b
MohammadArik <mohammadarik63@gmail.com>
2022-08-09 00:04:45 +0600

    new APIHandler implemented

--- lib/components/result_view/result_view.dart
+++ lib/components/result_view/result_view.dart
@@ -1,94 +1,97 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:provider/provider.dart';
 import 'package:wordlys/bloc/api_handler.dart';
 import 'package:wordlys/components/result_view/meaning_block.dart';
 import 'package:wordlys/components/result_view/sources_object.dart';
 import 'package:wordlys/const/colors.dart';
+import '../../models/word_data.dart';
 
 class ResultView extends StatelessWidget {
-  const ResultView({required this.apiHandler});
-  final APIHandler apiHandler;
+  const ResultView();
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        width: Size.infinite.width,
-        color: const Color(PRIMARY),
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        child: StreamBuilder<WordData>(
-          stream: apiHandler.getData,
-          builder: ((context, snapshot) {
-            var data = snapshot.data;
+    var data = context.watch<APIHandler>().data;
 
-            if (data != null && data.state != DataState.Empty) {
-              //* Check if loading
-              if (data.state == DataState.Loading) {
-                return Center(
-                  child: LoadingAnimationWidget.stretchedDots(
-                    color: const Color(HIGHLIGHT),
-                    size: 120,
-                  ),
-                );
-              }
-
-              //* Check if the result is not found
-              if (data.state == DataState.NotFound) {
-                return Text(
-                  "Can't find any results.",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(PRIMARY_TEXT),
-                    fontSize: 15,
-                  ),
-                );
-              }
-
-              //* It is now surely ready
-              // Parse the meaning-data and create MeaningBlock(s)
-              var meaningWidgets = <Widget>[];
-
-              for (var meaningBlock in data.meaningBlocksData) {
-                var partOfSpeech = meaningBlock["partOfSpeech"] as String;
-                var synonyms = meaningBlock["synonyms"] as List<dynamic>;
-                var antonyms = meaningBlock["antonyms"] as List<dynamic>;
-
-                var definitions = <String>[];
-                for (var definition in meaningBlock["definitions"]) {
-                  definitions.add(definition["definition"] as String);
-                }
-
-                meaningWidgets.add(MeaningBlock(
-                    definitions: definitions,
-                    antonyms: antonyms,
-                    synonyms: synonyms,
-                    partOfSpeech: partOfSpeech));
-              }
-
-              return SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      "Click to copy the synonyms and antonyms!",
-                      // softWrap: false,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                    const SizedBox(height: 4),
-                    ...meaningWidgets, // MeaningObject(s)
-                    SourcesObject(sources: data.sources)
-                  ],
-                ),
-              );
-            }
-
-            return SizedBox();
-          }),
+    //? Check if loading
+    if (data.state == DataState.Loading) {
+      return Center(
+        child: LoadingAnimationWidget.stretchedDots(
+          color: const Color(HIGHLIGHT),
+          size: 120,
         ),
-      ),
-    );
+      );
+    }
+
+    //? Check if the result is not found
+    if (data.state == DataState.NotFound) {
+      return Text(
+        "Can't find any results.",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Color(PRIMARY_TEXT),
+          fontSize: 15,
+        ),
+      );
+    }
+
+    //? Check if the result is not found
+    if (data.state == DataState.Error) {
+      return Text(
+        "Error loading results.\nPlease check your internet connection.",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Color(PRIMARY_TEXT),
+          fontSize: 15,
+        ),
+      );
+    }
+
+    //? If the data state is ready
+    if (data.state == DataState.Ready) {
+      // Parse the meaning-data and create MeaningBlock(s)
+      var meaningWidgets = <Widget>[];
+      for (var meaningBlock in data.meaningBlocksData) {
+        var partOfSpeech = meaningBlock["partOfSpeech"] as String;
+        var synonyms = meaningBlock["synonyms"] as List<dynamic>;
+        var antonyms = meaningBlock["antonyms"] as List<dynamic>;
+
+        var definitions = <String>[];
+        for (var definition in meaningBlock["definitions"]) {
+          definitions.add(definition["definition"] as String);
+        }
+
+        meaningWidgets.add(
+          MeaningBlock(
+            definitions: definitions,
+            antonyms: antonyms,
+            synonyms: synonyms,
+            partOfSpeech: partOfSpeech,
+          ),
+        );
+      }
+
+      return SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "Click to copy the synonyms and antonyms!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 4),
+            ...meaningWidgets, // MeaningObject(s)
+            SourcesObject(sources: data.sources) // the sources object
+          ],
+        ),
+      );
+    }
+
+    //? And lastly if data state is empty(which is default)
+    return const SizedBox();
   }
 }
